a5ff470f85d82ebec1c830f7799a574764a37154
oonbeh <oonbeh@gmail.com>
2023-01-01 15:31:04 +0800

    Update global.js

--- global.js
+++ global.js
@@ -5,7 +5,7 @@ const configAds = [
 		"target-selector":"body"
 	},
 	{
-		"src":"https://readydolphinpoverty.com/4a/00/1e/4a001e886418d57f6154e262bac9b581.js",
+		"src":"",
 		"name-element":"script",
 		"target-selector":"body"
 	}
