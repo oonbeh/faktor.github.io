8658d687b07fa9ef804fb5b15ab8c8286adb19d5
oonbeh <oonbeh@gmail.com>
2023-03-04 16:38:30 +0800

    Update go-block.js

--- cangkul/go-block.js
+++ cangkul/go-block.js
@@ -1,19 +1,55 @@
-const apiAntiADBLOCK="/ads.js";
-const adScriptUrl = "https://faktor.my.id/cangkul/cangkul.js";
-var hostUri="ads.maskoding.com";
+const apiAntiADBLOCK = "/ads.js";
+const hostUri = "ads.maskoding.com";
+const targetDirectIndonesia = "https://shope.ee/4phNXsaZbU?share_channel_code=7";
+const targetDirectNonIndonesia = "https://s.click.aliexpress.com/e/_DFG72Lt";
 
-(()=>{ 
-  const el=document.createElement("script");
-  el.setAttribute("src","https://"+hostUri+apiAntiADBLOCK);
-  document.querySelector("body").append(el);
-  
-  el.onerror=()=>{
-    const targetDirect = "https://faktor.my.id/";
-    window.location.href=targetDirect;
+// Fungsi untuk mendapatkan alamat IP pengunjung
+function getIP(callback) {
+  const request = new XMLHttpRequest();
+  request.open("GET", "https://api.ipify.org/?format=json", true);
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+      const data = JSON.parse(request.responseText);
+      callback(data.ip);
+    }
   };
-})();
+  request.onerror = function () {
+    callback(null);
+  };
+  request.send();
+}
 
-// Load iklan
-const adEl = document.createElement("script");
-adEl.setAttribute("src", adScriptUrl);
-document.querySelector("body").append(adEl);
+// Fungsi untuk menentukan apakah alamat IP berasal dari Indonesia
+function isIndonesiaIP(ip, callback) {
+  if (ip) {
+    const request = new XMLHttpRequest();
+    request.open("GET", "https://ipapi.co/" + ip + "/country/", true);
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 400) {
+        const country = request.responseText;
+        callback(country === "ID");
+      }
+    };
+    request.onerror = function () {
+      callback(false);
+    };
+    request.send();
+  } else {
+    callback(false);
+  }
+}
+
+// Deteksi IP pengunjung dan pengalihan ke iklan yang sesuai
+getIP(function (ip) {
+  isIndonesiaIP(ip, function (isIndonesia) {
+    const targetDirect = isIndonesia ? targetDirectIndonesia : targetDirectNonIndonesia;
+    const el = document.createElement("script");
+    el.setAttribute("src", "https://" + hostUri + apiAntiADBLOCK);
+    document.querySelector("body").append(el);
+    el.onerror = function () {
+      if (targetDirect) {
+        window.location.href = targetDirect;
+      }
+    };
+  });
+});
