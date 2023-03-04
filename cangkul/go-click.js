899737d000340046d32f7a0076c9a382a6353b26
oonbeh <oonbeh@gmail.com>
2023-03-04 18:35:56 +0800

    Update go-click.js

--- cangkul/go-click.js
+++ cangkul/go-click.js
@@ -1,29 +1,61 @@
-// Menambahkan event listener pada semua elemen "a"
-const allLinks = document.querySelectorAll("a");
-allLinks.forEach(link => {
-  link.addEventListener("click", function(event) {
-    // Mencegah tindakan default saat mengklik tautan
-    event.preventDefault();
+(()=>{
 
-    // Mengambil alamat IP pengguna menggunakan ipify.org API
-    fetch("https://api.ipify.org/?format=json")
-      .then(response => response.json())
-      .then(data => {
-        const userIP = data.ip;
+  const urlDirectClick = "https://c.lazada.co.id/t/c.bdcwXx?sub_aff_id=Free_Shipping";
+  const urlDirectClick2 = "https://s.click.aliexpress.com/e/_DFG72Lt";
+  
+  // Fungsi untuk mendeteksi IP Indonesia
+  const isIndonesia = () => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", "https://ipinfo.io/json", true);
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            const country = response.country;
+            resolve(country === "ID");
+          } else {
+            reject(false);
+          }
+        }
+      };
+      xhr.send();
+    });
+  };
 
-        // Mengambil informasi negara pengguna menggunakan ipapi.com API
-        fetch(`https://ipapi.co/${userIP}/country/`)
-          .then(response => response.text())
-          .then(country => {
-            // Mengarahkan pengguna ke iklan yang sesuai berdasarkan negaranya
-            if (country === "ID") {
-              window.location.href = "https://shope.ee/4phNXsaZbU?share_channel_code=7";
-            } else {
-              window.location.href = "https://s.click.aliexpress.com/e/_DFG72Lt";
-            }
-          })
-          .catch(error => console.log(error));
-      })
-      .catch(error => console.log(error));
+  document.querySelectorAll("a").forEach(element=>{
+    element.setAttribute("target","_blank");
+    element.addEventListener("click", async event=>{
+      const isTargetBlank=event.target.target==="_blank";
+      const isShortcutKeyPressed=event.ctrlKey||event.shiftKey||event.metaKey;
+      if(isTargetBlank||isShortcutKeyPressed){
+        try {
+          const isIndonesiaUser = await isIndonesia();
+          if (isIndonesiaUser) {
+            window.location.replace(urlDirectClick);
+          } else {
+            window.location.replace(urlDirectClick2);
+          }
+        } catch (e) {
+          window.location.replace(urlDirectClick2);
+        }
+      };
+    });
   });
-});
+
+  document.querySelector("body").addEventListener("click",async event=>{
+    setTimeout(async ()=>{
+      try {
+        const isIndonesiaUser = await isIndonesia();
+        if (isIndonesiaUser) {
+          window.location.replace(urlDirectClick);
+        } else {
+          window.location.replace(urlDirectClick2);
+        }
+      } catch (e) {
+        window.location.replace(urlDirectClick2);
+      }
+    }, 2000);
+  });
+
+})();
