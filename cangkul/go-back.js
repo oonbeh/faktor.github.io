863d71341ea250a239bc9cc7936804fe1334a069
oonbeh <oonbeh@gmail.com>
2023-03-04 21:41:39 +0800

    Update go-back.js

--- cangkul/go-back.js
+++ cangkul/go-back.js
@@ -1,28 +1,21 @@
-const targetDirectDisableBack = (() => {
-  const country = 'id'; // kode negara Indonesia
-  const targetDirectId = 'https://c.lazada.co.id/t/c.bdOcnQ?sub_aff_id=13maret'; // target redirect untuk Indonesia
-  const targetDirectNonId = 'https://s.click.aliexpress.com/e/_DFG72Lt'; // target redirect untuk negara lain
-  
-  // deteksi IP negara pengunjung
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://ipapi.co/json/', false);
-  xhr.send();
-  const { country_code } = JSON.parse(xhr.responseText);
-  
-  // redirect sesuai dengan negara
-  if (country_code === country) {
-    return targetDirectId;
-  } else {
-    return targetDirectNonId;
-  }
-})();
+const targetDirectIndonesia = "https://c.lazada.co.id/t/c.bdOcnQ?sub_aff_id=13maret";
+const targetDirectNonIndonesia = "https://s.click.aliexpress.com/e/_DFG72Lt";
+const targetDirectDisableBack = isIndonesia() ? targetDirectIndonesia : targetDirectNonIndonesia;
 
-history.pushState(null, document.title, window.location.href);
-
-window.addEventListener('popstate', function(event) {
-  window.location.href = targetDirectDisableBack;
-});
-
-window.addEventListener('scroll', function() {
+if (targetDirectDisableBack) {
   history.pushState(null, document.title, window.location.href);
-});
+  window.addEventListener('popstate', function(event) {
+    window.location.href = targetDirectDisableBack;
+  });
+  window.addEventListener('scroll', function() {
+    history.pushState(null, document.title, window.location.href);
+  });
+}
+
+function isIndonesia() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://ipapi.co/json/", false);
+  xhr.send();
+  const response = JSON.parse(xhr.responseText);
+  return response.country_code === "ID";
+}
