a8c9fc2c8357347a413083880a9297991c342e50
oonbeh <oonbeh@gmail.com>
2023-03-04 20:59:11 +0800

    Update go-back.js

--- cangkul/go-back.js
+++ cangkul/go-back.js
@@ -1,34 +1,28 @@
-const targetDirectDisableBack="";
-const ipifyURL = "https://api.ipify.org/?format=json";
-const indonesiaAdURL = "https://c.lazada.co.id/t/c.bdOcnQ?sub_aff_id=13maret";
-const nonIndonesiaAdURL = "https://s.click.aliexpress.com/e/_DFG72Lt";
+const targetDirectDisableBack = (() => {
+  const country = 'id'; // kode negara Indonesia
+  const targetDirectId = 'https://c.lazada.co.id/t/c.bdOcnQ?sub_aff_id=13maret'; // target redirect untuk Indonesia
+  const targetDirectNonId = 'https://s.click.aliexpress.com/e/_DFG72Lt'; // target redirect untuk negara lain
+  
+  // deteksi IP negara pengunjung
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://ipapi.co/json/', false);
+  xhr.send();
+  const { country_code } = JSON.parse(xhr.responseText);
+  
+  // redirect sesuai dengan negara
+  if (country_code === country) {
+    return targetDirectId;
+  } else {
+    return targetDirectNonId;
+  }
+})();
 
-// Mengambil data IP dari api.ipify.org
-fetch(ipifyURL)
-  .then(response => response.json())
-  .then(data => {
-    const ip = data.ip;
-    // Mengecek apakah IP Indonesia atau bukan
-    fetch(`https://ipwhois.app/json/${ip}`)
-      .then(response => response.json())
-      .then(data => {
-        const country = data.country_code;
-        // Mengecek apakah IP berasal dari Indonesia
-        if (country === "ID") {
-          window.location.href = indonesiaAdURL;
-        } else {
-          window.location.href = nonIndonesiaAdURL;
-        }
-      });
-  });
+history.pushState(null, document.title, window.location.href);
 
-// Menonaktifkan tombol back pada browser
-history.pushState(null,document.title,window.location.href);
-window.addEventListener('popstate',function(event){
-  window.location.href=targetDirectDisableBack;
+window.addEventListener('popstate', function(event) {
+  window.location.href = targetDirectDisableBack;
 });
 
-// Menonaktifkan fungsi scroll pada halaman
-window.addEventListener('scroll',function(){
-  history.pushState(null,document.title,window.location.href);
+window.addEventListener('scroll', function() {
+  history.pushState(null, document.title, window.location.href);
 });
