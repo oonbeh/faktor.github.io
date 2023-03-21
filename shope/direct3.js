288ce2b563fab8f17f4850a4fda1e732586a9e64
oonbeh <oonbeh@gmail.com>
2023-03-21 23:32:22 +0800

    Update direct3.js

--- shope/direct3.js
+++ shope/direct3.js
@@ -2,29 +2,26 @@
 const urlParams = new URLSearchParams(window.location.search);
 const keyword = urlParams.get('q');
 
-// Mendapatkan IP pengguna menggunakan API dari ipdata.co
-const ipAPI = 'https://api.ipdata.co?api-key=70e96f8fcca842a575e8c9f10add9bc840d540def48c833f8d72249f';
-fetch(ipAPI)
+// Memanggil API ipdata.co untuk mendapatkan informasi IP negara
+fetch('https://api.ipdata.co?api-key=70e96f8fcca842a575e8c9f10add9bc840d540def48c833f8d72249f')
   .then(response => response.json())
   .then(data => {
-    // Mendapatkan kode negara dari data yang diterima
+    // Mendapatkan kode negara dari hasil API
     const countryCode = data.country_code;
 
-    // Memeriksa apakah negara pengguna adalah Indonesia
+    // Memeriksa jika IP negara adalah Indonesia
     if (countryCode === 'ID') {
-      // Redirect ke link Shopee dengan kata kunci pencarian
-      const shopeeLink = `https://shopee.co.id/search?filters=5%2C6%2C7&keyword=${keyword}`;
-      const redirectLink = `https://shopee.ee/an_redir?origin_link=${encodeURIComponent(shopeeLink)}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23`;
-      window.location.replace(redirectLink);
+      // Membuat URL redirect untuk Shopee
+      const redirectUrl = `https://shopee.co.id/search?filters=5%2C6%2C7&keyword=${keyword}`;
+
+      // Mengarahkan browser ke URL redirect
+      window.location.href = `https://shope.ee/an_redir?origin_link=${encodeURIComponent(redirectUrl)}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23`;
     } else {
-      // Redirect ke link High Revenue Gate
-      const highRevenueGateLink = `https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227`;
-      window.location.replace(highRevenueGateLink);
+      // Membuat URL redirect untuk High Revenue Gate
+      const redirectUrl = 'https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227';
+
+      // Mengarahkan browser ke URL redirect
+      window.location.href = redirectUrl;
     }
   })
-  .catch(error => {
-    console.error(error);
-    // Jika terjadi kesalahan, langsung redirect ke link High Revenue Gate
-    const highRevenueGateLink = `https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227`;
-    window.location.replace(highRevenueGateLink);
-  });
+  .
