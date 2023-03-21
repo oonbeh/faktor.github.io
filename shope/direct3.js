27ff125bb7524d6af411a10685c79d3c899d4a6d
oonbeh <oonbeh@gmail.com>
2023-03-21 23:48:06 +0800

    Update direct3.js

--- shope/direct3.js
+++ shope/direct3.js
@@ -1,26 +1,29 @@
-// Mendapatkan URL saat ini
+// mengambil url saat ini
 const currentUrl = window.location.href;
 
-// Mengekstrak kata kunci pencarian dari URL
-const searchParams = new URLSearchParams(currentUrl.split('?')[1]);
-const keyword = searchParams.get('q');
+// mengambil parameter pencarian dari url
+const searchParam = new URLSearchParams(currentUrl.split('?')[1]);
 
-// Mendapatkan IP negara menggunakan ipdata.co
-fetch('https://api.ipdata.co/?api-key=70e96f8fcca842a575e8c9f10add9bc840d540def48c833f8d72249f')
+// mendapatkan kata kunci pencarian dari parameter pencarian
+const keyword = searchParam.get('q');
+
+// mengambil informasi IP negara pengunjung dari ipdata.co
+fetch('https://api.ipdata.co/?api-key=<API_KEY>')
   .then(response => response.json())
   .then(data => {
+    // mendapatkan kode negara dari data IP
     const countryCode = data.country_code;
 
-    // Jika negara adalah Indonesia, maka arahkan ke shopee.co.id
+    // memeriksa apakah kode negara pengunjung adalah Indonesia
     if (countryCode === 'ID') {
-      const redirectUrl = `https://shopee.co.id/search?filters=5%2C6%2C7&keyword=${keyword}`;
-      window.location.replace(`https://shope.ee/an_redir?origin_link=${encodeURIComponent(redirectUrl)}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23`);
-    }
-    // Jika negara bukan Indonesia, maka arahkan ke highrevenuegate.com
-    else {
-      window.location.replace('https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227');
+      // mengarahkan ke link Shopee jika negara Indonesia
+      const shopeeLink = `https://shopee.co.id/search?filters=5%2C6%2C7&keyword=${encodeURIComponent(keyword)}`;
+      const redirectLink = `https://shope.ee/an_redir?origin_link=${encodeURIComponent(shopeeLink)}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23`;
+      window.location.replace(redirectLink);
+    } else {
+      // mengarahkan ke link High Revenue Gate jika negara bukan Indonesia
+      const highrevenuegateLink = 'https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227';
+      window.location.replace(highrevenuegateLink);
     }
   })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  .catch(error => console.log(error));
