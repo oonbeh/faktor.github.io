4a20b2b9634b0587bdac923d7b6492a2bfe14246
oonbeh <oonbeh@gmail.com>
2023-03-21 22:58:06 +0800

    Update direct3.js

--- shope/direct3.js
+++ shope/direct3.js
@@ -1,31 +1,22 @@
-// Mendapatkan kata kunci pencarian dari URL
-const searchParams = new URLSearchParams(window.location.search);
-const keyword = searchParams.get("q");
+// mengambil kata kunci pencarian dari URL
+const urlParams = new URLSearchParams(window.location.search);
+const keyword = urlParams.get('q');
 
-// Fungsi untuk mengalihkan pengguna ke tautan yang sesuai berdasarkan IP negara
-function redirectBasedOnCountry() {
-  fetch("https://ipapi.co/json/")
-    .then((response) => response.json())
-    .then((data) => {
-      const countryCode = data.country_code;
+// mendapatkan kode negara dari IP address
+const getCountryCode = async () => {
+  const response = await fetch('https://api.ipdata.co/country_code?api-key=test');
+  const countryCode = await response.text();
+  return countryCode;
+}
+const countryCode = await getCountryCode();
 
-      if (countryCode === "ID") {
-        const shopeeLink =
-          "https://shopee.co.id/search?filters=5%2C6%2C7&keyword=" + keyword;
-        const redirectLink =
-          "https://shopee.ee/an_redir?origin_link=" +
-          encodeURIComponent(shopeeLink) +
-          "&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23";
-        window.location.href = redirectLink;
-      } else {
-        const redirectLink = "https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227";
-        window.location.href = redirectLink;
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+// menentukan URL redirect berdasarkan negara
+let redirectUrl;
+if (countryCode === 'ID') {
+  redirectUrl = `https://shope.ee/an_redir?origin_link=https%3A%2F%2Fshopee.co.id%2Fsearch%3Ffilters%3D5%252C6%252C7%26keyword%3D${keyword}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23`;
+} else {
+  redirectUrl = 'https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227';
 }
 
-// Memanggil fungsi untuk mengalihkan pengguna
-redirectBasedOnCountry();
+// melakukan redirect ke URL yang ditentukan
+window.location.href = redirectUrl;
