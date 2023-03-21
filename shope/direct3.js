7fc90efb23a0ecdf265adb14439c7c057b2bea5d
oonbeh <oonbeh@gmail.com>
2023-03-21 22:47:57 +0800

    Update direct3.js

--- shope/direct3.js
+++ shope/direct3.js
@@ -1,18 +1,31 @@
-// Ambil URL saat ini
-const currentURL = window.location.href;
+// Mendapatkan kata kunci pencarian dari URL
+const searchParams = new URLSearchParams(window.location.search);
+const keyword = searchParams.get("q");
 
-// Periksa apakah pengguna mengakses dari IP local atau bukan
-const isLocal = /(^127\.0\.0\.1)|(^192\.168\.)/.test(window.location.hostname);
+// Fungsi untuk mengalihkan pengguna ke tautan yang sesuai berdasarkan IP negara
+function redirectBasedOnCountry() {
+  fetch("https://ipapi.co/json/")
+    .then((response) => response.json())
+    .then((data) => {
+      const countryCode = data.country_code;
 
-// Tentukan tautan arah yang akan digunakan berdasarkan status lokal atau tidak
-let redirectLink = '';
-if (isLocal) {
-  const keyword = currentURL.match(/q=(.*)/)[1];
-  redirectLink = `https://shopee.co.id/search?filters=5%2C6%2C7&keyword=${keyword}`;
-  redirectLink = `https://shopee.ee/an_redir?origin_link=${encodeURIComponent(redirectLink)}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23`;
-} else {
-  redirectLink = 'https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227';
+      if (countryCode === "ID") {
+        const shopeeLink =
+          "https://shopee.co.id/search?filters=5%2C6%2C7&keyword=" + keyword;
+        const redirectLink =
+          "https://shopee.ee/an_redir?origin_link=" +
+          encodeURIComponent(shopeeLink) +
+          "&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23";
+        window.location.href = redirectLink;
+      } else {
+        const redirectLink = "https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227";
+        window.location.href = redirectLink;
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-// Arahkan pengguna ke tautan arah yang sesuai
-window.location.href = redirectLink;
+// Memanggil fungsi untuk mengalihkan pengguna
+redirectBasedOnCountry();
