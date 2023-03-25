9ad2db32ccff1f732d90dc776ec2c3a98f9de03e
oonbeh <oonbeh@gmail.com>
2023-03-26 07:23:45 +0800

    Update direct8.js

--- shope/direct8.js
+++ shope/direct8.js
@@ -1,30 +1,30 @@
-// Ambil parameter dari URL
+// Mendapatkan parameter dari URL
 const urlParams = new URLSearchParams(window.location.search);
-const queryParam = urlParams.get('q');
-const slugParam = urlParams.get('slug');
 
-// Fungsi untuk mendapatkan informasi IP dari ipwhois
-function getCountryFromIP(ip) {
-  return fetch(`https://ipwhois.app/json/${ip}`)
-    .then(response => response.json())
-    .then(data => data.country_code)
-    .catch(error => console.log(error));
-}
+// Mengambil nilai parameter 'q' atau 'slug-url.html'
+const searchParam = urlParams.get('q');
+const slugParam = window.location.pathname.substring(1);
 
-// Redirect URL berdasarkan negara pengguna
-async function redirectByCountry() {
-  const response = await fetch('https://api.ipify.org/?format=json');
-  const data = await response.json();
-  const countryCode = await getCountryFromIP(data.ip);
+// Mendapatkan IP pengguna menggunakan ipwhois
+fetch('https://ipwhois.app/json/')
+  .then(response => response.json())
+  .then(data => {
+    // Mendapatkan kode negara dari lokasi IP
+    const countryCode = data.country_code;
 
-  if (countryCode === 'ID') {
-    const keyword = queryParam || slugParam;
-    const redirectURL = `https://shopee.co.id/search?filters=5%2C6%2C7&keyword=${encodeURIComponent(keyword)}`;
-    window.location.href = `https://shopee.ee/an_redir?origin_link=${encodeURIComponent(redirectURL)}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23`;
-  } else {
-    window.location.href = 'https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227';
-  }
-}
-
-// Jalankan fungsi untuk melakukan redirect
-redirectByCountry();
+    // Jika negara adalah Indonesia, maka redirect ke link Shopee
+    if (countryCode === 'ID') {
+      const keyword = searchParam ? `&keyword=${searchParam}` : slugParam ? `&keyword=${slugParam}` : '';
+      const shopeeLink = `https://shopee.co.id/search?filters=5%2C6%2C7${keyword}`;
+      const redirectLink = `https://shopee.ee/an_redir?origin_link=${encodeURIComponent(shopeeLink)}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23`;
+      window.location.href = redirectLink;
+    } 
+    // Jika negara bukan Indonesia, maka redirect ke link HighRevenueGate
+    else {
+      const redirectLink = 'https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227';
+      window.location.href = redirectLink;
+    }
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
