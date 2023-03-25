6c98a33c9f4cc744591fb67c8c3c756357a7efd6
oonbeh <oonbeh@gmail.com>
2023-03-26 06:32:28 +0800

    Update direct6.js

--- shope/direct6.js
+++ shope/direct6.js
@@ -1,17 +1,33 @@
-var searchUrl = window.location.href;
-var searchQuery = searchUrl.split('=')[1];
-var ipUrl = "https://ipwhois.app/json/";
+// 1. Mendapatkan kata kunci pencarian dari URL
+var keyword = '';
+var url = window.location.href;
+if (url.includes('/search/q=')) {
+  keyword = url.split('/search/q=')[1].split('&')[0];
+}
 
-fetch(ipUrl)
-.then(response => response.json())
-.then(data => {
-  if(data.country === 'Indonesia') {
-    var redirectUrl = "https://shopee.co.id/search?filters=5%2C6%2C7&keyword=" + searchQuery;
-    window.location.href = "https://shope.ee/an_redir?origin_link=" + encodeURIComponent(redirectUrl) + "&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23";
+// 2. Mendapatkan IP negara menggunakan ipwhois
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://ipwhois.app/json/', true);
+xhr.onload = function () {
+  if (xhr.status === 200) {
+    var response = JSON.parse(xhr.responseText);
+    var countryCode = response.country_code;
+    
+    // 3. Jika IP negara Indonesia, redirect ke https://shope.ee/an_redir?origin_link=...
+    if (countryCode === 'ID') {
+      var shopeeUrl = 'https://shopee.co.id/search?filters=5%2C6%2C7&keyword=' + keyword;
+      var redirectUrl = 'https://shope.ee/an_redir?origin_link=' + encodeURIComponent(shopeeUrl) + '&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23';
+      window.location.replace(redirectUrl);
+    } else {
+      // 4. Jika IP bukan negara Indonesia, redirect ke https://www.highrevenuegate.com/ix4w14mn00?key=...
+      var redirectUrl = 'https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227';
+      window.location.replace(redirectUrl);
+    }
   } else {
-    window.location.href = "https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227";
+    console.error(xhr.statusText);
   }
-})
-.catch(error => {
-  console.error('Error:', error);
-});
+};
+xhr.onerror = function () {
+  console.error(xhr.statusText);
+};
+xhr.send();
