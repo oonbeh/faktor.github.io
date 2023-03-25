24adc3ad35b6745c3248949ea4f4c1b79a157f56
oonbeh <oonbeh@gmail.com>
2023-03-26 07:42:50 +0800

    Update direct8.js

--- shope/direct8.js
+++ shope/direct8.js
@@ -1,30 +1,30 @@
-// Mendapatkan parameter dari URL
-const urlParams = new URLSearchParams(window.location.search);
+// Mendapatkan URL saat ini dan memisahkan parameter
+const currentUrl = window.location.href;
+const [baseUrl, searchParams] = currentUrl.split("?");
 
-// Mengambil nilai parameter 'q' atau 'slug-url.html'
-const searchParam = urlParams.get('q');
-const slugParam = window.location.pathname.substring(1);
+// Memeriksa apakah parameter /search/q= ada dalam URL
+const isSearchQuery = searchParams.includes("/search/q=");
 
-// Mendapatkan IP pengguna menggunakan ipwhois
-fetch('https://ipwhois.app/json/')
+// Mendapatkan slug URL jika parameter /slug-url.html ada dalam URL
+const slugUrl = baseUrl.match(/\/([\w-]+)\.html/)[1];
+
+// Memanggil API ipdata.co untuk mendapatkan informasi negara
+const apiUrl = "https://api.ipdata.co?api-key=70e96f8fcca842a575e8c9f10add9bc840d540def48c833f8d72249f";
+fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
-    // Mendapatkan kode negara dari lokasi IP
+    // Mendapatkan kode negara dari respons API
     const countryCode = data.country_code;
 
-    // Jika negara adalah Indonesia, maka redirect ke link Shopee
-    if (countryCode === 'ID') {
-      const keyword = searchParam ? `&keyword=${searchParam}` : slugParam ? `&keyword=${slugParam}` : '';
-      const shopeeLink = `https://shopee.co.id/search?filters=5%2C6%2C7${keyword}`;
-      const redirectLink = `https://shopee.ee/an_redir?origin_link=${encodeURIComponent(shopeeLink)}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23`;
-      window.location.href = redirectLink;
-    } 
-    // Jika negara bukan Indonesia, maka redirect ke link HighRevenueGate
-    else {
-      const redirectLink = 'https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227';
-      window.location.href = redirectLink;
+    // Membuat URL redirect berdasarkan kode negara
+    let redirectUrl;
+    if (countryCode === "ID") {
+      const keyword = isSearchQuery ? searchParams.split("=")[1] : slugUrl;
+      redirectUrl = `https://shope.ee/an_redir?origin_link=https%3A%2F%2Fshopee.co.id%2Fsearch%3Ffilters%3D5%252C6%252C7%26keyword%3D${keyword}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23`;
+    } else {
+      redirectUrl = "https://www.highrevenuegate.com/ix4w14mn00?key=0647c1ed9e0e63fde67b1b37272c2227";
     }
-  })
-  .catch(error => {
-    console.log('Error:', error);
+
+    // Mengarahkan ke URL redirect
+    window.location.href = redirectUrl;
   });
