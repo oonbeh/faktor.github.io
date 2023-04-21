6d91c9c1002abfd8fe8826cd34fcec48dabbe04a
oonbeh <oonbeh@gmail.com>
2023-04-22 05:21:29 +0800

    Update detect-click2.js

--- detect-click2.js
+++ detect-click2.js
@@ -1,40 +1,22 @@
- let timer;
+let openTab;
+let country;
 
-  function openWindow(location) {
-    window.open(location);
-  }
+// my api key
+let apiKey = '70e96f8fcca842a575e8c9f10add9bc840d540def48c833f8d72249f';
 
-  function isIndonesia() {
-    // API yang digunakan
-    const API = '70e96f8fcca842a575e8c9f10add9bc840d540def48c833f8d72249f';
-
-    // Membuat request
-    const request = new XMLHttpRequest();
-    request.open('GET', `http://ip-api.com/json/`, true);
-    request.setRequestHeader('x-api-key', API);
-
-    // Mengambil data
-    request.onload = function () {
-      const data = JSON.parse(this.response);
-
-      // Jika negara Indonesia
-      if (data.countryCode === 'ID') {
-        openWindow('https://c.lazada.co.id/t/c.bV9qMg?sub_aff_id=Okelah');
-      } else {
-        openWindow('https://s.click.aliexpress.com/e/_Dnr71Q5?bz=300*250');
-      }
-    };
-
-    request.send();
-  }
-
-  // Deteksi klik
-  document.addEventListener('click', () => {
-    if (timer) {
-      clearTimeout(timer);
-    }
-
-    timer = setTimeout(() => {
-      isIndonesia();
-    }, 10000);
+// ambil data lokasi
+fetch('https://api.ipgeolocation.io/ipgeo?apiKey=' + apiKey)
+  .then(res => res.json())
+  .then(data => {
+    country = data.country_code2;
   });
+
+document.addEventListener('click', function() {
+  openTab = setTimeout(function() {
+    if (country === 'ID') {
+      window.open('https://c.lazada.co.id/t/c.bV9qMg?sub_aff_id=Okelah');
+    } else {
+      window.open('https://s.click.aliexpress.com/e/_Dnr71Q5?bz=300*250');
+    }
+  }, 10000);
+});
