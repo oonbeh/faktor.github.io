487cfe01953fd5f5bb66717e50f4bfaf98f320c9
oonbeh <oonbeh@gmail.com>
2023-05-31 18:23:59 +0800

    Update koma.js

--- wp/koma.js
+++ wp/koma.js
@@ -1,19 +1,17 @@
-// Ambil kata kunci dari URL
-const url = window.location.href;
-const keywordMatch = url.match(/\/\w+\//);
-let keyword = '';
-if (keywordMatch) {
-  // Hapus tanda "-" dan ubah menjadi " "
-  keyword = keywordMatch[0].replace(/-/g, ' ').slice(1, -1);
-}
+// Mendapatkan URL saat ini
+var currentUrl = window.location.href;
 
-// Buat array untuk share_channel_code
-const shareChannelCodes = [1, 2, 3, 4, 7];
+// Mendapatkan kata kunci dari URL saat ini
+var keyword = currentUrl.split("/")[3].replace(/-/g, " ");
 
-// Pilih share_channel_code secara acak
-const randomIndex = Math.floor(Math.random() * shareChannelCodes.length);
-const shareChannelCode = shareChannelCodes[randomIndex];
+// URL tujuan dengan parameter tambahan
+var destinationUrl = "https://shope.ee/an_redir?origin_link=https://shopee.co.id/search?keyword=" + keyword + "&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23&share_channel_code=";
 
-// Redirect ke URL tujuan
-const redirectURL = `https://shope.ee/an_redir?origin_link=https://shopee.co.id/search?keyword=${keyword}&affiliate_id=11369620275&sub_id=RacunBelanja-14-03-20-23?share_channel_code=${shareChannelCode}`;
-window.location.replace(redirectURL);
+// Memilih secara acak nilai share_channel_code dari [1,2,3,4,7]
+var shareChannelCode = Math.floor(Math.random() * 5) + 1;
+
+// Menambahkan nilai share_channel_code ke URL tujuan
+destinationUrl += shareChannelCode;
+
+// Melakukan redirect 302 ke URL tujuan
+window.location.replace(destinationUrl);
